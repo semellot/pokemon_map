e03d8bcbd209f8a5445e10456486abce646c6dd6
Olga <o.panova27@gmail.com>
2022-08-22 22:00:18 +0300

    Change mandatory fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -17,17 +17,17 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     """Сущность покемона"""
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_DEFAULT,
-        default=1, blank=True, verbose_name="Покемон")
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL,
+        null=True, verbose_name="Покемон")
     lat = models.FloatField(verbose_name="Координата широты")
     lon = models.FloatField(verbose_name="Координата долготы")
     appeared_at = models.DateTimeField(null=True, verbose_name="Дата и время появления")
     disappeared_at = models.DateTimeField(null=True, verbose_name="Дата и время исчезновения")
-    level = models.IntegerField(null=True, verbose_name="Уровень")
-    health = models.IntegerField(null=True, verbose_name="Здоровье")
-    strength = models.IntegerField(null=True, verbose_name="Сила")
-    defence = models.IntegerField(null=True, verbose_name="Защита")
-    stamina = models.IntegerField(null=True, verbose_name="Выносливость")
+    level = models.IntegerField(blank=True, verbose_name="Уровень")
+    health = models.IntegerField(blank=True, verbose_name="Здоровье")
+    strength = models.IntegerField(blank=True, verbose_name="Сила")
+    defence = models.IntegerField(blank=True, verbose_name="Защита")
+    stamina = models.IntegerField(blank=True, verbose_name="Выносливость")
 
     def __str__(self):
         return f'{self.pokemon}: {self.lat} - {self.lon}'
